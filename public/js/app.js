701dd4e289440d29db0af50af6899a920fd7e673
John Carlo Ferreras <johncarloferreras@gmail.com>
2022-10-09 23:16:28 +0800

    Edited weather description

--- public/js/app.js
+++ public/js/app.js
@@ -7,7 +7,7 @@ weatherForm.addEventListener('submit', (e) => {
   e.preventDefault();
 
   const location = searchElement.value;
-  messageOne.textContent = 'Please wait...';
+  messageOne.textContent = 'Please wait as we are trying to retrieve weather information.';
   fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((weather) => {
       messageOne.textContent = '';
@@ -15,7 +15,7 @@ weatherForm.addEventListener('submit', (e) => {
         messageOne.textContent = weather.error;
         messageTwo.textContent = '';
       } else {
-        messageOne.textContent = `The current weather in ${weather.address} is ${weather.temperature}`;
+        messageOne.textContent = `The current weather in ${weather.address} is ${weather.temperature} but feels like ${weather.feelslike}. It is ${weather.weatherdesc}.`;
         messageTwo.textContent = '';
       }
     });
