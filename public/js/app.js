f4fbec5d481c2195fade3eb1eb7b446d556aa5d2
John Carlo Ferreras <johncarloferreras@gmail.com>
2022-10-09 22:56:11 +0800

    Preparation for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,3 @@
-fetch('https://puzzle.mead.io/puzzle').then((response) => {
-  response.json().then((data) => {
-    console.log(data);
-  });
-});
-
 const weatherForm = document.querySelector('form');
 const searchElement = document.querySelector('input');
 const messageOne = document.querySelector('#weatherOne');
@@ -14,7 +8,7 @@ weatherForm.addEventListener('submit', (e) => {
 
   const location = searchElement.value;
   messageOne.textContent = 'Please wait...';
-  fetch(`http://localhost:3000/weather?address=${location}`).then((response) => {
+  fetch(`/weather?address=${location}`).then((response) => {
     response.json().then((weather) => {
       messageOne.textContent = '';
       if (weather.error) {
